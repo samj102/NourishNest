383e1b4f2c82e9f37220304bbbeeaf7d716eb292
James <77467136+jtcarom@users.noreply.github.com>
2023-11-02 20:28:06 -0400

    backend - Models created/modified
    Recipe model modified to contain ingredients and steps.
    SavedRecipes and Scheduled models created.

--- backend/nourishnest/api/models.py
+++ backend/nourishnest/api/models.py
@@ -1,39 +1,27 @@
 from django.db import models
 from django.db.models import UniqueConstraint
-from django.db.models.functions import Lower
-
+from django.contrib.auth.models import User
 # Create your models here.
 
-# User Model
-
-
-#Weekly Schedule
-
-# Saved Recipes
-
-
-# Recipe Models:
+# Recipe Model:
 class Recipe(models.Model):
     name = models.CharField(max_length=50, null=False, editable=True)
-    cuisine = models.CharField(max_length=50, null=True, editable=True)
+    cuisine = models.CharField(max_length=50, null=False, editable=True)
+    ingredients = models.TextField()
+    steps = models.TextField()
 
-
-class Ingredient(models.Model):
-    name = models.CharField(max_length=50, null=False, editable=True)
-    quantity = models.PositiveSmallIntegerField() # max 32767
-    recipe = models.ForeignKey(Recipe, db_index=True, on_delete=models.CASCADE)
-    
+# Saved Recipes
+class SavedRecipes(models.Model):
+    recipe = models.ForeignKey(Recipe, null=False, db_index=True, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, null=False, db_index=True, on_delete=models.CASCADE)
     class Meta:
-        constraints = [
-            models.UniqueConstraint('name', 'recipe', name='ingredient_recipe_unique')
-        ]
+        UniqueConstraint('recipe', 'user', name="unique_user_saved_recipe")
 
-class RecipeStep(models.Model):
-    step = models.CharField(max_length=100, null=False, editable=True)
-    step_number = models.PositiveSmallIntegerField() # max 32767
-    recipe = models.ForeignKey(Recipe, db_index=True, on_delete=models.CASCADE)
-
+#Weekly Schedule
+class Scheduled(models.Model):
+    recipe = models.ForeignKey(Recipe, null=True, db_index=True, on_delete=models.CASCADE, editable=True)
+    user = models.ForeignKey(User, null=False, db_index=True, on_delete=models.CASCADE)
+    day = models.CharField(null=False, max_length=9)
+    meal = models.CharField(null=False, max_length=10)
     class Meta:
-        constraints = [
-            models.UniqueConstraint('step_number', 'recipe', name='order_recipe_unique')
-        ]
+        UniqueConstraint('user', 'day', 'meal', name="unique_user_scheduled_meal")
--- backend/nourishnest/api/serializers.py
+++ backend/nourishnest/api/serializers.py
@@ -1,20 +1,7 @@
 from rest_framework import serializers
-from .models import Recipe, Ingredient, RecipeStep
-'''
-class IngredientSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Ingredient
-        fields = '__all__'
+from .models import Recipe
 
-class RecipeStepSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = RecipeStep
-        fields = '__all__'
-'''
 class RecipeSerializer(serializers.ModelSerializer):
-    #recipe_ingredients = IngredientSerializer(many=True)
-    #recipe_steps = RecipeStepSerializer(many=True)
-
     class Meta:
         model = Recipe
         fields = '__all__'
