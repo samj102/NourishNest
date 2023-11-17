a6f362e90bdbf2cd51ca4cc32bcd1d54e4d3612f
Brandon Ly <git@brndn.ly>
2023-11-17 09:15:50 -0500

    feat: 404 not found page

--- frontend/nourishnest/src/App.jsx
+++ frontend/nourishnest/src/App.jsx
@@ -2,24 +2,31 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/navbar';
 import Footer from './components/footer';
 import Index from './pages/index';
+import Login from './pages/login';
+import NotFound from './pages/NotFound';
 import './App.css';
 import {Box} from "@mui/material";
+import {AuthProvider} from "./AuthContext.jsx";
 
 
 
 function App() {
     return (
-        <Router>
-            <Box sx={{ display: 'flex', flexDirection: 'column', minHeight: '100vh' }}>
-                <Navbar />
-                <Box component={'main'} sx={{ flexGrow: 1, mt: 4 }}>
-                    <Routes>
-                        <Route path="/" element={<Index/>}/>
-                    </Routes>
+        <AuthProvider>
+            <Router>
+                <Box sx={{ display: 'flex', flexDirection: 'column', minHeight: '100vh' }}>
+                    <Navbar />
+                    <Box component={'main'} sx={{ flexGrow: 1, mt: 4 }}>
+                        <Routes>
+                            <Route path="/" element={<Index/>}/>
+                            <Route path="/login" element={<Login/>}/>
+                            <Route path="*" element={<NotFound/>}/>
+                        </Routes>
+                    </Box>
+                    <Footer/>
                 </Box>
-                <Footer/>
-            </Box>
-        </Router>
+            </Router>
+        </AuthProvider>
     );
 }
 
