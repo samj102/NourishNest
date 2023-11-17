55ec40f27b2010cee97852f8d4fac031121c3cf6
Brandon Ly <git@brndn.ly>
2023-11-17 10:35:31 -0500

    feat: login page authentication

--- frontend/nourishnest/src/components/navbar.jsx
+++ frontend/nourishnest/src/components/navbar.jsx
@@ -1,22 +1,24 @@
 import { AppBar, Toolbar, Button } from '@mui/material';
 import { Link as Link, useNavigate } from 'react-router-dom';
 import TextLogo from './TextLogo.jsx';
+import { AuthContext } from '../AuthContext.jsx';
+import { useContext } from 'react';
 
 const Navbar = () => {
-    // Check if CSRF token is present in localStorage
-    const isLoggedIn = !!localStorage.getItem('crsfToken');
-    const navigate = useNavigate();
+    // Check if authentication cookie exists
+    const { isAuthenticated, setIsAuthenticated } = useContext(AuthContext);
 
     const handleLogout = () => {
-        localStorage.removeItem('crsfToken');
-        navigate('/');
-    }
+        // Perform logout logic here
+        setIsAuthenticated(false);
+        // Remove session cookie or call API to invalidate the session
+    };
 
     return (
         <AppBar style={{ background: '#f3f4f6', boxShadow: '0px 3px 5px rgba(0,0,0,0.2)' }}>
             <Toolbar>
                 <TextLogo/>
-                {isLoggedIn ? (
+                {isAuthenticated ? (
                     <>
                         <Button style={{ color: "black", marginRight: '0.4em'}} no wrap variant={"text"} component={Link} to="/my-recipes">My Recipes</Button>
                         <Button style={{ color: "black", marginRight: '0.4em'}} variant={"text"} component={Link} to="/planner">Planner</Button>
--- frontend/nourishnest/src/pages/login.jsx
+++ frontend/nourishnest/src/pages/login.jsx
@@ -1,30 +1,101 @@
-import React from "react";
-import {
-  Container,
-  Box,
-  Typography,
-  Button,
-  Grid,
-  useTheme,
-  useMediaQuery,
-} from "@mui/material";
-import { Link as RouterLink } from "react-router-dom";
-import TestimonialCard from "../components/testimonialCard.jsx";
-import Logo from "../assets/logo.png";
+import {Alert, Box, Button, Checkbox, Container, FormControlLabel, Link, TextField, Typography} from "@mui/material";
+import {Link as RouterLink, useNavigate} from "react-router-dom";
+import {useState} from "react";
 
 const Login = () => {
-  const theme = useTheme();
-  const isSmallScreen = useMediaQuery(theme.breakpoints.down("sm"));
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+    const [error, setError] = useState('');
+    const navigate = useNavigate();
 
-  // Style for images to maintain equal height with text content
-  const imageStyle = isSmallScreen
-    ? {
-        width: "100%",
-        boxShadow: "0px 3px 5px rgba(0,0,0,0.2)",
-        alignSelf: "center",
-        marginBottom: "3em",
-      }
-    : { width: "80%", boxShadow: "0px 3px 5px rgba(0,0,0,0.2)" };
+    function loginUser(credentials) {
+        // csrf protection
+        function getCookie(name) {
+            let cookieValue = null;
+            if (document.cookie && document.cookie !== '') {
+                const cookies = document.cookie.split(';');
+                for (let i = 0; i < cookies.length; i++) {
+                    const cookie = cookies[i].trim();
+                    if (cookie.substring(0, name.length + 1) === (name + '=')) {
+                        cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                        break;
+                    }
+                }
+            }
+            return cookieValue;
+        }
+
+        const csrftoken = getCookie('csrftoken');
+
+        return fetch('http://localhost:8000/api/login', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'X-CSRFToken': csrftoken,
+            },
+            body: JSON.stringify(credentials),
+            credentials: 'include'
+        })
+            .then(response => {
+                // Check if the response is ok (status in the range 200-299)
+                if (!response.ok) {
+                    // Convert non-2xx HTTP responses into errors:
+                    return response.json().then(data => Promise.reject(data));
+                }
+                return response.json();
+            })
+            .catch(err => {
+                // Handle network errors and json parsing errors
+                console.error("Error", err);
+                throw err; // Re-throwing the error so it can be caught by the caller
+            });
+    }
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        setError(""); // Clear previous errors
+        try {
+            const response = await loginUser({
+                "username": username,
+                "password": password,
+            });
+            navigate("/"); // Redirect to home page on successful login
+        } catch (err) {
+            setError(err.message || "Failed to login"); // Display error message from server
+        }
+    };
+
+
+    return (
+        <Container component={'main'} maxWidth={'xs'}>
+            <Box sx={{ marginTop: 10, display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+                <Typography variant={'h2'}>
+                    Login
+                </Typography>
+                <Box component={'form'} noValidate sx={{ mt: 1 }} onSubmit={handleSubmit}>
+                    <TextField margin={'normal'} required fullWidth id={'username'} label={'Username'} name={'username'} autoComplete={'username'} autoFocus value={username}
+                               onChange={(e) => setUsername(e.target.value)}/>
+                    <TextField margin={'normal'} required fullWidth name={'password'} label={'Password'} type={'password'} id={'password'} autoComplete={'current-password'} value={password}
+                        onChange={(e) => setPassword(e.target.value)}/>
+                    <FormControlLabel control={<Checkbox value={'remember'} color={'primary'}/>} label={'Remember me'}/>
+
+                    {/* Display error message */}
+                    {error && (
+                        <Alert severity="error" sx={{ width: '100%', mt: 2 }}>
+                            {error}
+                        </Alert>
+                    )}
+
+
+                    <Button type={'submit'} fullWidth variant={'contained'} sx={{ mt: 3, mb: 2 }}>Login</Button>
+
+                    <Link href={'#'} variant={'body2'}>
+                        <RouterLink to={'/register'}>Don't have an account? Sign Up</RouterLink>
+                    </Link>
+                </Box>
+            </Box>
+        </Container>
+    );
 };
 
 export default Login;
