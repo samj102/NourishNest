6f75513466a602c7121590d872cc5c1a9d2c9c2e
samj102 <s1jafri@ryerson.ca>
2023-11-30 15:10:11 -0500

    Register Page Created
    Created the UI components of the register page for now, there is a problem with the synchronous function with no quick fixes, to be discussed later. Minor visual changes made to the login page

--- frontend/nourishnest/src/pages/login.jsx
+++ frontend/nourishnest/src/pages/login.jsx
@@ -1,91 +1,138 @@
-import {Alert, Box, Button, Checkbox, Container, FormControlLabel, Link, TextField, Typography} from "@mui/material";
-import {Link as RouterLink, useNavigate} from "react-router-dom";
-import {useContext, useState} from "react";
-import {getCookie, getCSRFToken} from "../utils";
+import {
+  Alert,
+  Box,
+  Button,
+  Checkbox,
+  Container,
+  FormControlLabel,
+  Link,
+  TextField,
+  Typography,
+} from "@mui/material";
+import { Link as RouterLink, useNavigate } from "react-router-dom";
+import { useContext, useState } from "react";
+import { getCookie, getCSRFToken } from "../utils";
 import { AuthContext } from "../components/authContext";
 
 const Login = () => {
-    const { isAuthenticated, login } = useContext(AuthContext);
+  const { isAuthenticated, login } = useContext(AuthContext);
 
-    const [username, setUsername] = useState('');
-    const [password, setPassword] = useState('');
-    const [error, setError] = useState('');
-    const navigate = useNavigate();
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+  const navigate = useNavigate();
 
-    function loginUser(credentials) {
-
-        return fetch('http://localhost:8000/api/login', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-                'X-CSRFToken': getCSRFToken(),
-            },
-            body: JSON.stringify(credentials),
-            credentials: 'include'
-        })
-            .then(response => {
-                // Check if the response is ok (status in the range 200-299)
-                if (!response.ok) {
-                    // Convert non-2xx HTTP responses into errors:
-                    return response.json().then(data => Promise.reject(data));
-                }
-                return response.json();
-            })
-            .catch(err => {
-                // Handle network errors and json parsing errors
-                console.error("Error", err);
-                throw err; // Re-throwing the error so it can be caught by the caller
-            });
-    }
-
-    const handleSubmit = async (e) => {
-        e.preventDefault();
-        setError(""); // Clear previous errors
-        try {
-            const response = await loginUser({
-                "username": username,
-                "password": password,
-            });
-            login(response.username);
-            navigate("/"); // Redirect to home page on successful login
-        } catch (err) {
-            setError(err.message || "Failed to login"); // Display error message from server
+  function loginUser(credentials) {
+    return fetch("http://localhost:8000/api/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "X-CSRFToken": getCSRFToken(),
+      },
+      body: JSON.stringify(credentials),
+      credentials: "include",
+    })
+      .then((response) => {
+        // Check if the response is ok (status in the range 200-299)
+        if (!response.ok) {
+          // Convert non-2xx HTTP responses into errors:
+          return response.json().then((data) => Promise.reject(data));
         }
-    };
+        return response.json();
+      })
+      .catch((err) => {
+        // Handle network errors and json parsing errors
+        console.error("Error", err);
+        throw err; // Re-throwing the error so it can be caught by the caller
+      });
+  }
 
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setError(""); // Clear previous errors
+    try {
+      const response = await loginUser({
+        username: username,
+        password: password,
+      });
+      login(response.username);
+      navigate("/"); // Redirect to home page on successful login
+    } catch (err) {
+      setError(err.message || "Failed to login"); // Display error message from server
+    }
+  };
 
-    return (
-        <Container component={'main'} maxWidth={'xs'}>
-            <Box sx={{ marginTop: 10, display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
-                <Typography variant={'h2'}>
-                    Login
-                </Typography>
-                <Box component={'form'} noValidate sx={{ mt: 1 }} onSubmit={handleSubmit}>
-                    <TextField margin={'normal'} required fullWidth id={'username'} label={'Username'} name={'username'} autoComplete={'username'} autoFocus value={username}
-                               onChange={(e) => setUsername(e.target.value)}/>
-                    <TextField margin={'normal'} required fullWidth name={'password'} label={'Password'} type={'password'} id={'password'} autoComplete={'current-password'} value={password}
-                        onChange={(e) => setPassword(e.target.value)}/>
-                    <FormControlLabel control={<Checkbox value={'remember'} color={'primary'}/>} label={'Remember me'}/>
+  return (
+    <Container component={"main"} maxWidth={"xs"}>
+      <Box
+        sx={{
+          marginTop: 10,
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+        }}
+      >
+        <Typography variant={"h2"}>Login</Typography>
+        <Box
+          component={"form"}
+          noValidate
+          sx={{ mt: 1 }}
+          onSubmit={handleSubmit}
+        >
+          <TextField
+            margin={"normal"}
+            required
+            fullWidth
+            id={"username"}
+            label={"Username"}
+            name={"username"}
+            autoComplete={"username"}
+            autoFocus
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+          <TextField
+            margin={"normal"}
+            required
+            fullWidth
+            name={"password"}
+            label={"Password"}
+            type={"password"}
+            id={"password"}
+            autoComplete={"current-password"}
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <FormControlLabel
+            control={<Checkbox value={"remember"} color={"primary"} />}
+            label={"Remember me"}
+          />
 
-                    {/* Display error message */}
-                    {error && (
-                        <Alert severity="error" sx={{ mt: 2 }}>
-                            {error}
-                        </Alert>
-                    )}
+          {/* Display error message */}
+          {error && (
+            <Alert severity="error" sx={{ mt: 2 }}>
+              {error}
+            </Alert>
+          )}
 
+          <Button
+            type={"submit"}
+            fullWidth
+            variant={"contained"}
+            sx={{ mt: 3, mb: 2 }}
+          >
+            Login
+          </Button>
 
-                    <Button type={'submit'} fullWidth variant={'contained'} sx={{ mt: 3, mb: 2 }}>Login</Button>
-
-                    <RouterLink to={'/register'}>
-                        <Typography variant='p'>
-                            Don't have an account? Sign Up
-                        </Typography>
-                    </RouterLink>
-                </Box>
-            </Box>
-        </Container>
-    );
+          <RouterLink to={"/register"}>
+            <Typography variant="p" color={"black"}>
+              Don't have an account? Sign Up
+            </Typography>
+          </RouterLink>
+        </Box>
+      </Box>
+    </Container>
+  );
 };
 
 export default Login;
--- frontend/nourishnest/src/pages/register.jsx
+++ frontend/nourishnest/src/pages/register.jsx
@@ -1,67 +1,143 @@
-import {Box, Button, Container, Typography} from "@mui/material";
-import {Link as RouterLink, useNavigate} from 'react-router-dom';
-import {useState} from "react";
-import {getCSRFToken} from "../utils.js";
+import {
+  Alert,
+  Box,
+  Button,
+  Checkbox,
+  Container,
+  FormControlLabel,
+  Link,
+  TextField,
+  Typography,
+} from "@mui/material";
+import { Link as RouterLink, useNavigate } from "react-router-dom";
+import { useState } from "react";
+import { getCSRFToken } from "../utils.js";
 
 const Register = () => {
-    const [email, setEmail] = useState('');
-    const [username, setUsername] = useState('');
-    const [password, setPassword] = useState('');
-    const [error, setError] = useState('');
-    const navigate = useNavigate();
+  const [email, setEmail] = useState("");
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+  const navigate = useNavigate();
 
-    function registerUser(credentials) {
-
-        return fetch('http://localhost:8000/api/register', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-                'X-CSRFToken': getCSRFToken(),
-            },
-            body: JSON.stringify(credentials),
-            credentials: 'include'
-        })
-            .then(response => {
-                // Check if the response is ok (status in the range 200-299)
-                if (!response.ok) {
-                    // Convert non-2xx HTTP responses into errors:
-                    return response.json().then(data => Promise.reject(data));
-                }
-                return response.json();
-            })
-            .catch(err => {
-                // Handle network errors and json parsing errors
-                console.error("Error", err);
-                throw err; // Re-throwing the error so it can be caught by the caller
-            });
-    }
-
-    const handleSubmit = async (e) => {
-        e.preventDefault();
-        setError(""); // Clear previous errors
-        try {
-            const response = await registerUser({
-                "email": email,
-                "username": username,
-                "password": password,
-            });
-            localStorage.setItem('isAuthenticated', "true");
-            navigate("/"); // Redirect to home page on successful login
-        } catch (err) {
-            setError(err.message || "Failed to register"); // Display error message from server
+  function registerUser(credentials) {
+    return fetch("http://localhost:8000/api/register", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "X-CSRFToken": getCSRFToken(),
+      },
+      body: JSON.stringify(credentials),
+      credentials: "include",
+    })
+      .then((response) => {
+        // Check if the response is ok (status in the range 200-299)
+        if (!response.ok) {
+          // Convert non-2xx HTTP responses into errors:
+          return response.json().then((data) => Promise.reject(data));
         }
-    };
+        return response.json();
+      })
+      .catch((err) => {
+        // Handle network errors and json parsing errors
+        console.error("Error", err);
+        throw err; // Re-throwing the error so it can be caught by the caller
+      });
+  }
 
-    return (
-        <Container component={'main'} maxWidth={'sm'} >
-            <Box sx={{ mt: '16%', textAlign: 'center' }}>
-                <Typography variant="h2" gutterBottom>
-                    Register
-                </Typography>
+  // const handleSubmit = async (e) => {
+  //   e.preventDefault();
+  //   setError(""); // Clear previous errors
 
-            </Box>
-        </Container>
-    )
-}
+  //   }
+  //   try {
+  //     const response = await registerUser({
+  //       email: email,
+  //       username: username,
+  //       password: password,
+  //     });
+  //     localStorage.setItem("isAuthenticated", "true");
+  //     navigate("/"); // Redirect to home page on successful login
+  //   } catch (err) {
+  //     setError(err.message || "Failed to register"); // Display error message from server
+  //   }
+  // };
+
+  return (
+    <Container component={"main"} maxWidth={"sm"}>
+      <Box sx={{ mt: "16%", textAlign: "center" }}>
+        <Typography variant="h2" gutterBottom>
+          Register Page
+        </Typography>
+
+        <Box
+          component={"form"}
+          noValidate
+          sx={{ mt: 1 }}
+          //   onSubmit={handleSubmit}
+        >
+          {/* Email Input */}
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            id="email"
+            label="Email Address"
+            name="email"
+            autoComplete="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          {/* Username Input */}
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            id="username"
+            label="Username"
+            name="username"
+            autoComplete="username"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+          {/* Password Input */}
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            name="password"
+            label="Password"
+            type="password"
+            id="password"
+            autoComplete="new-password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+
+          {error && (
+            <Alert severity="error" sx={{ mt: 2 }}>
+              {error}
+            </Alert>
+          )}
+          {/* Register Button */}
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            sx={{ mt: 3, mb: 2 }}
+          >
+            Register
+          </Button>
+          {/* Link to Login Page */}
+          <RouterLink to="/login">
+            <Typography variant="p" color={"black"}>
+              Already have an account? Login
+            </Typography>
+          </RouterLink>
+        </Box>
+      </Box>
+    </Container>
+  );
+};
 
 export default Register;
