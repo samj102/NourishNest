a74de88ce7d14530a1283d9e5b5475c63cace6f6
samj102 <s1jafri@ryerson.ca>
2023-12-01 15:59:33 -0500

    Updated My-Recipes Page

--- frontend/nourishnest/src/pages/my-recipes.jsx
+++ frontend/nourishnest/src/pages/my-recipes.jsx
@@ -1,14 +1,36 @@
-import {Box, Container, Typography} from "@mui/material";
-
+import {
+  Alert,
+  Box,
+  Button,
+  Checkbox,
+  Container,
+  FormControlLabel,
+  Link,
+  TextField,
+  Typography,
+} from "@mui/material";
+import { Link as RouterLink, useNavigate } from "react-router-dom";
+import { useContext, useState } from "react";
+import { getCookie, getCSRFToken } from "../utils";
+import { AuthContext } from "../components/authContext";
 
 const MyRecipes = () => {
-    return (
-        <Container component={'main'} maxWidth={'lg'}>
-            <Box sx={{display: 'flex', flexDirection: 'column', alignItems: 'center', marginTop: 10}}>
-                <Typography variant={'h3'} sx={{mt: 4, mb: 2}}>My Recipes</Typography>
-            </Box>
-        </Container>
-    );
-}
+  return (
+    <Container component={"main"} maxWidth={"lg"}>
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          marginTop: 10,
+        }}
+      >
+        <Typography variant={"h3"} sx={{ mt: 4, mb: 2 }}>
+          My Recipes
+        </Typography>
+      </Box>
+    </Container>
+  );
+};
 
 export default MyRecipes;
