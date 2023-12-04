a4d940a4ab6afc15bc798a019eef693fdc7fd9db
samj102 <s1jafri@ryerson.ca>
2023-12-04 04:01:07 -0500

    My Recipes Page Updated

--- frontend/nourishnest/src/pages/my-recipes.jsx
+++ frontend/nourishnest/src/pages/my-recipes.jsx
@@ -1,84 +1,27 @@
-// import {
-//     Alert,
-//     Box,
-//     Button,
-//     Checkbox,
-//     Container, Fab,
-//     FormControlLabel,
-//     Link,
-//     TextField,
-//     Typography,
-// } from "@mui/material";
-// import { Link as RouterLink, useNavigate } from "react-router-dom";
-// import { useContext, useState } from "react";
-// import { getCookie, getCSRFToken } from "../utils";
-// import { AuthContext } from "../components/authContext";
-// import AddIcon from '@mui/icons-material/Add';
-
-// const MyRecipes = () => {
-//   return (
-//     <Container component={"main"} maxWidth={"lg"}>
-//       <Box
-//         sx={{
-//           display: "flex",
-//           flexDirection: "column",
-//           alignItems: "center",
-//           marginTop: 10,
-//         }}
-//       >
-//         <Typography variant={"h3"} sx={{ mt: 4, mb: 2 }}>
-//           My Recipes
-//         </Typography>
-
-//         <Link
-//             component={RouterLink}
-//             to={"/create-recipe"}
-//             variant={"body1"}
-//             sx={{ m: 4 }}
-//         >
-//            <Fab color="primary" aria-label="add" sx={{
-//                 position: 'fixed',
-//                 bottom: '15rem',
-//                 right: 16,
-//                animation: 'pulse 2s infinite',
-//            }}>
-//                 <AddIcon />
-//            </Fab>
-//         </Link>
-
-//       </Box>
-//     </Container>
-//   );
-// };
-
-// export default MyRecipes;
-
 import { useState, useEffect, useContext } from "react";
-import {
-  Box,
-  Button,
-  Container,
-  Typography,
-} from "@mui/material";
+import { Box, Button, Container, Typography } from "@mui/material";
 import { Link as RouterLink } from "react-router-dom";
 import { Link as MuiLink } from "@mui/material";
 import { AuthContext } from "../components/authContext";
 import AddIcon from "@mui/icons-material/Add";
+import { getCookie, getCSRFToken } from "../utils";
 
 const MyRecipes = () => {
-  const { user } = useContext(AuthContext);
-  const userId = user ? user.id : null;
-
   const [recipes, setRecipes] = useState([]);
 
   useEffect(() => {
-    if (userId) {
-      fetch(`http://localhost:8000/api/users/${userId}/recipes`)
-        .then((response) => response.json())
-        .then((data) => setRecipes(data))
-        .catch((error) => console.error("Error fetching recipes", error));
-    }
-  }, [userId]);
+    fetch(`http://localhost:8000/api/savedrecipes`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        "X-CSRFToken": getCSRFToken(),
+      },
+      credentials: "include",
+    })
+      .then((response) => response.json())
+      .then((data) => setRecipes(data))
+      .catch((error) => console.error("Error fetching recipes", error));
+  });
 
   return (
     <Container component={"main"} maxWidth={"lg"}>
