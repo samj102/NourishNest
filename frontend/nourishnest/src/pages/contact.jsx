22c4d16639875c0cf6c0df449475939f8735d123
Brandon Ly <git@brndn.ly>
2023-12-04 02:02:30 -0500

    feat: contact page and mobile navbar fix

--- frontend/nourishnest/src/pages/contact.jsx
+++ frontend/nourishnest/src/pages/contact.jsx
@@ -1,11 +1,17 @@
-import {Box, Container, Typography} from "@mui/material";
+import {Box, Button, Container, TextField, Typography} from "@mui/material";
 
 
 const Contact = () => {
     return (
         <Container component={'main'} maxWidth={'lg'}>
-            <Box sx={{display: 'flex', flexDirection: 'column', alignItems: 'center', margin: 10}}>
-                <Typography variant={'h3'} sx={{m: 4}}>Contact</Typography>
+            <Box sx={{display: 'flex', flexDirection: 'column', alignItems: 'center', margin: 5}}>
+                <Typography variant={'h3'} sx={{mt: 4}}>Contact Us</Typography>
+
+                <Typography variant={'body1'} sx={{m: 4}}>Fill out the form below and we'll definitely get back to you.</Typography>
+                <TextField variant={'outlined'} label={'Name'} sx={{m: 2, width: "20em"}}/>
+                <TextField variant={'outlined'} label={'Email'} sx={{m: 2, width: "20em"}}/>
+                <TextField variant={'outlined'} label={'Message'} sx={{m: 2, width: "20em"}} multiline rows={4}/>
+                <Button variant={'contained'} sx={{m: 2}}>Submit</Button>
 
             </Box>
         </Container>
