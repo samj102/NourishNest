e602af84279db41e551768aba704295e7a29380c
samj102 <s1jafri@ryerson.ca>
2023-12-04 06:07:25 -0500

    Updated Browse Recipes

--- frontend/nourishnest/src/pages/browse-recipes.jsx
+++ frontend/nourishnest/src/pages/browse-recipes.jsx
@@ -1,51 +1,34 @@
 import React, { useState, useEffect } from "react";
-import {
-  Box,
-  Button,
-  Container,
-  Grid,
-  Typography,
-  TextField,
-} from "@mui/material";
+import { Box, Button, Container, Grid, Typography } from "@mui/material";
 import { Link as RouterLink } from "react-router-dom";
-import AddIcon from "@mui/icons-material/Add";
 import ImageIcon from "@mui/icons-material/Image";
 import { getCSRFToken } from "../utils";
 
 const BrowseRecipes = () => {
   const [recipes, setRecipes] = useState([]);
-  const [filter, setFilter] = useState("");
+  const [filterCuisine, setFilterCuisine] = useState("");
 
   const handleSearch = () => {
     const csrfToken = getCSRFToken();
 
-    fetch(`http://localhost:8000/api/globalrecipes`, {
+    // You can modify the URL to include the filterCuisine parameter
+    fetch(`http://localhost:8000/api/globalrecipes?cuisine=${filterCuisine}`, {
       method: "GET",
       headers: {
         "Content-Type": "application/json",
-        "X-CSRFToken": csrfToken, // Include CSRF token in the header
-      },
-      credentials: "include",
-    })
-      .then((response) => response.json())
-      .then((data) => setRecipes(data))
-      .catch((error) =>
-        console.error("Error fetching filtered recipes", error)
-      );
-  };
-
-  useEffect(() => {
-    fetch("http://localhost:8000/api/globalrecipes", {
-      method: "GET",
-      headers: {
-        "Content-Type": "application/json",
+        "X-CSRFToken": csrfToken,
       },
       credentials: "include",
     })
       .then((response) => response.json())
       .then((data) => setRecipes(data))
       .catch((error) => console.error("Error fetching recipes", error));
-  }, []);
+  };
+
+  useEffect(() => {
+    // Fetch initial recipes when the component mounts
+    handleSearch();
+  }, []); // Empty dependency array means this effect runs once after the initial render
 
   return (
     <Container component={"main"} maxWidth={"lg"}>
@@ -61,18 +44,19 @@ const BrowseRecipes = () => {
           Browse Recipes
         </Typography>
 
-        <Box sx={{ marginBottom: 2 }}>
-          <TextField
-            label="Filter Cuisine"
-            variant="outlined"
-            value={filter}
-            onChange={(e) => setFilter(e.target.value)}
+        {/* Add a search input and button */}
+        <Box sx={{ mb: 3 }}>
+          <input
+            type="text"
+            placeholder="Filter cuisine"
+            value={filterCuisine}
+            onChange={(e) => setFilterCuisine(e.target.value)}
           />
           <Button
+            onClick={handleSearch}
             variant="contained"
             color="primary"
-            onClick={handleSearch}
-            sx={{ marginLeft: 2 }}
+            sx={{ ml: 2 }}
           >
             Search
           </Button>
@@ -80,7 +64,46 @@ const BrowseRecipes = () => {
 
         <Grid container spacing={3} justifyContent="center">
           {recipes.map((recipe) => (
-            <Grid item key={recipe.id} xs={12} md={4}></Grid>
+            <Grid item key={recipe.id} xs={12} md={4}>
+              <Box sx={{ p: 2, border: "1px solid #ddd", borderRadius: 8 }}>
+                <Typography variant="h5" sx={{ mb: 1 }}>
+                  {recipe.name}
+                </Typography>
+                {recipe.image ? (
+                  <img
+                    src={recipe.image}
+                    alt={recipe.name}
+                    style={{
+                      maxWidth: "100%",
+                      height: "auto",
+                      borderRadius: 8,
+                    }}
+                  />
+                ) : (
+                  <div
+                    style={{
+                      backgroundColor: "white",
+                      width: "100%",
+                      height: "150px",
+                      borderRadius: 8,
+                      display: "flex",
+                      justifyContent: "center",
+                      alignItems: "center",
+                    }}
+                  >
+                    <ImageIcon fontSize={"large"} />
+                  </div>
+                )}
+                <Button
+                  component={RouterLink}
+                  to={`/view-recipe/${recipe.id}`}
+                  variant="outlined"
+                  sx={{ mt: 2 }}
+                >
+                  View Recipe
+                </Button>
+              </Box>
+            </Grid>
           ))}
         </Grid>
       </Box>
